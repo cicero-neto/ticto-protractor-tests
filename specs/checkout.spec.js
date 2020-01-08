addcf8d427ad964d9b1a0f9d3581e8d527e7bcb7
Cicero <cicero.neto@ticto.com.br>
2020-01-08 11:32:44 -0300

    Adicionado implicityWait para esperar o checkout carregar completamente

--- specs/checkout.spec.js
+++ specs/checkout.spec.js
@@ -3,29 +3,24 @@ browser.waitForAngularEnabled(false);
 let Checkout = require('../page-objects/checkout.po');
 
 describe ('New Checkout', () => {
+    browser.manage().timeouts().implicitlyWait(5000);
 
     it('try to submit personal data form', () => {
-        Checkout.visit('https://payb.ticto.com.br/c/B82F17C1');
-        browser.driver.sleep(5000);
+        Checkout.visit('http://dev.pay.ticto.com.br/c/3C0FAF67');
 
         Checkout.personalDataForm('John Doe', 'john.doe@', '123456789', '12345678901');
 
         expect($('.invalid-feedback').isPresent()).toBe(true);
-
-        browser.driver.sleep(3000);
     });
 
     it('try to submit personal data form and go to Payment tab', () => {
-        Checkout.visit('https://payb.ticto.com.br/c/B82F17C1');
-        browser.driver.sleep(5000);
+        Checkout.visit('http://dev.pay.ticto.com.br/c/3C0FAF67');
 
-        Checkout.personalDataForm('John Doe', 'john.doe@gmail.com', '12345678900', '12345678901');
+        Checkout.personalDataForm('Cicero Neto', 'cicero.testeux@gmail.com', '12345678900', '34181356841');
 
         let title = $('#personal-data > div > div > div > div.col-sm-12.col-lg-8.remove-padding.background-form.shadow.border-style > div > div:nth-child(4) > div > div > div:nth-child(1) > div > h3')
 
-        expect(title.getText()).not.toContain('Pagamento')
-
-        browser.driver.sleep(3000);
+        expect(title.getText()).toContain('Pagamento')
     });
 });
 
