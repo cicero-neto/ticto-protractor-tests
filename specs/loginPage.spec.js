9b3ec7b7f0518d8e4d954a7910563b4e20f7a4b0
Cicero <cicero.neto@ticto.com.br>
2020-01-08 14:17:58 -0300

    Adicionado mais testes para a tela de login

--- specs/loginPage.spec.js
+++ specs/loginPage.spec.js
@@ -3,21 +3,78 @@ browser.waitForAngularEnabled(false);
 let login = browser.params.login;
 let LoginPage = require('../page-objects/loginPage.po');
 
-describe ('Login App Ticto', () => {
+describe('Login App Ticto', () => {
+    browser.manage().timeouts().implicitlyWait(5000);
+
+    it('should password message error', () => {
+        let loginPage = new LoginPage();
+
+        let email = login.user,
+            password = login.password,
+            text = 'O campo senha é obrigatório.',
+            passwordError = $('.validation-message .error');
+
+        loginPage.visit();
+
+        loginPage.signIn(email, '');
+
+        expect(passwordError.isPresent()).toBe(true);
+        expect(passwordError.getText()).toContain(text);
+    });
+
+    it('should email message error', () => {
+        let loginPage = new LoginPage();
+
+        let email = login.user,
+            password = login.password,
+            text = 'O campo E-mail é obrigatório.',
+            emailError = $('.validation-message .error');
+
+        loginPage.visit();
+
+        loginPage.signIn('', password);
+
+        expect(emailError.isPresent()).toBe(true);
+        expect(emailError.getText()).toContain(text);
+    });
+
+    it('should email or password message error', () => {
+        let loginPage = new LoginPage();
+
+        let email = login.user,
+            password = login.password,
+            text = 'Email ou senha incorretos.',
+            emailError = $('.validation-message .error');
+
+        loginPage.visit();
+
+        loginPage.signIn(email, 'password');
+
+        expect(emailError.isPresent()).toBe(true);
+        expect(emailError.getText()).toContain(text);
+    });
 
     it('should login app', () => {
         let loginPage = new LoginPage();
-        
-        let email = login.user, 
+
+        let email = login.user,
             password = login.password;
 
         loginPage.visit();
 
-        browser.driver.sleep(3000);
         loginPage.signIn(email, password);
         browser.driver.sleep(3000);
 
         expect($('.profile-picture').isPresent()).toBe(true);
+    });
+
+    it('should logout app', () => {
+        $('#userbox .fa.custom-caret').click();
+        browser.driver.sleep(3000);
+
+        $('#userbox > div > ul > li:nth-child(4) a').click();
+
+        expect($('.panel-title-sign .title').getText()).toContain('LOGIN');
     });
 });
 
